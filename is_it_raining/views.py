4a2b82c1b5ba67cb1b2ea52b6fd34b337a41fef3
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-04-17 22:33:55 -0400

    changed Animal model, working on view to apply weather to animal

--- is_it_raining/views.py
+++ is_it_raining/views.py
@@ -13,9 +13,24 @@ def api_root(request, format=None):
 
 
 class WeatherAnimalView(APIView):
-    def get(request, *args, **kwargs):
+    '''get weather code, link code with animal, create if none
+    '''
+
+    def get(self, request, *args, **kwargs):
         # get code from url for kwarg
         weather_code = self.kwargs["weather_code"]
+
         # search weather using code, create if none
         weather, created = Weather.objects.get_or_create(
             weather_code=weather_code)
+
+        animal_names = ["lizard", "rabbit", "centaur"]
+
+        # if it's true that it had to create it
+        if created:
+            animal = Animal(name=animal_names[0], weather=weather)
+            animal.save()
+        else:
+            animal = Animal.objects.get(weather=weather)
+
+        return Response({"animal": animal.name})
