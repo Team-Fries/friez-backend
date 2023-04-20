65c10a80a93364eaee8bbf27a8ae7531aff955f8
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-04-19 23:04:30 -0400

    added delete to capture animal view

--- is_it_raining/views.py
+++ is_it_raining/views.py
@@ -37,21 +37,33 @@ class AnimalDetailView(generics.RetrieveAPIView):
 
 
 class CapturedAnimalView(APIView):
+    '''logged in user captures animal or removes an animal
+    '''
 
     def post(self, request, name):
         owner = request.user
-
-        animal = get_object_or_404(Animal, name=name)
+        animal = get_object_or_404(Animal, name__iexact=name)
 
         captured = CapturedAnimal.objects.create(owner=owner, animal=animal)
-
         serializer = CapturedAnimalSerializer(captured)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def delete(self, request, name):
+        owner = request.user
+        animal = get_object_or_404(Animal, name__iexact=name)
+
+        release_animal = get_object_or_404(
+            CapturedAnimal, owner=owner, animal=animal)
+        release_animal.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class UserAnimalList(generics.ListAPIView):
     serializer_class = CapturedAnimalSerializer
+    ''' list of all the users caugh animals
+    '''
 
     def get_queryset(self):
         owner = self.request.user
